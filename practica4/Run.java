92319787303682ab8574782ae6e0094f5b8668ee
jire980604 <jire980604@gs.utm.mx>
2024-07-24 18:00:30 -0600

    styleCorrec

--- practica4/Run.java
+++ practica4/Run.java
@@ -3,14 +3,18 @@ public class Run{
         EdadException edEx = new EdadException();
 
 		try {
-            int edad =11;
+            int edad =11/0;
             edEx.validarEdad(edad);
-        } catch (Exception ex) {
+        } catch (NullPointerException ex){
             ex.printStackTrace();
             System.out.println("Entre al catch");
             System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }finally{
             System.out.println("finally");
         }
+
+        System.out.println("Continuacion del programa en java");
 	}
 }
